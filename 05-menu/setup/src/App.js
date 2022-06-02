9937831e9036b1e5b56c5d8648c1934a70d297b2
Gustavo dos Santos <gustavogugueira@gmail.com>
2022-06-02 16:12:23 -0300

    feat: Menu items

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,33 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItens , setMenuItens] = useState(items);
+  const [categories , setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItens(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItens(newItems);
+  }
+  return (
+    <main>
+        <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <Categories filterItems={filterItems} categories={categories} />
+          <Menu items={menuItens} />
+        </section>
+    </main>
+    )
 }
 
 export default App;
